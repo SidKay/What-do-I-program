bbb90bf23dc112fb4dc852060dfc0fab63e6dbb9
SidKay <sidkay1234@gmail.com>
2023-09-27 11:40:04 +0100

    View and template stuff #2

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 
 from . import views
 
+# Use URLconf namespacing when working with multiple apps.
+# This lets your templates know which url to use.
+app_name = 'polls'
 urlpatterns = [
 	path("", views.index, name="index"),
 	path('<int:question_id>/', views.detail, name='detail'),
